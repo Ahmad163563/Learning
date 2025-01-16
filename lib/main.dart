95df2a8ad590f20737c58d43ceb74aa7c58efa1e
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-01-16 09:56:58 +0500

    update in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,61 @@
-import "package:flutter/material.dart";
-
+// import "package:flutter/material.dart";
+//
+// void main()
+// {
+//   runApp(
+//     MyApp()
+//   );
+// }
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Colors.blue,
+//           title: Text('View Page', ),
+//           actions: [
+//             Icon(Icons.add, size:40),
+//             Icon(Icons.search, size:30),
+//             Icon(Icons.messenger, size:30),
+//             Icon(Icons.mail)
+//           ],
+//         ),
+//         body:
+//
+//         // Row(
+//         //   children: [
+//         //     CircleAvatar(child: Icon(Icons.home)),
+//         //     SizedBox(width: 50,),
+//         //     CircleAvatar(child: Icon(Icons.video_collection)),
+//         //     SizedBox(width: 50,),
+//         //     CircleAvatar(child: Icon(Icons.group)),
+//         //     SizedBox(width: 50,),
+//         //     CircleAvatar(child: Icon(Icons.mark_email_unread)),
+//         //     SizedBox(width: 50,),
+//         //     CircleAvatar(child: Icon(Icons.notification_important)),
+//         //     SizedBox(width: 50,),
+//         //     CircleAvatar(child: Icon(Icons.menu)),
+//         //   ],
+//         // )
+//         Center(
+//           child: CircleAvatar(
+//             radius: 100,
+//             backgroundColor: Colors.green,
+//             child: Text('data'),
+//
+//
+//           ),
+//         )
+//       ),
+//     );
+//
+//   }
+// }
+import 'package:flutter/material.dart';
 void main()
 {
   runApp(
@@ -12,46 +68,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: Text('View Page', ),
-          actions: [
-            Icon(Icons.add, size:40),
-            Icon(Icons.search, size:30),
-            Icon(Icons.messenger, size:30),
-            Icon(Icons.mail)
-          ],
-        ),
-        body:
-
-        // Row(
-        //   children: [
-        //     CircleAvatar(child: Icon(Icons.home)),
-        //     SizedBox(width: 50,),
-        //     CircleAvatar(child: Icon(Icons.video_collection)),
-        //     SizedBox(width: 50,),
-        //     CircleAvatar(child: Icon(Icons.group)),
-        //     SizedBox(width: 50,),
-        //     CircleAvatar(child: Icon(Icons.mark_email_unread)),
-        //     SizedBox(width: 50,),
-        //     CircleAvatar(child: Icon(Icons.notification_important)),
-        //     SizedBox(width: 50,),
-        //     CircleAvatar(child: Icon(Icons.menu)),
-        //   ],
-        // )
-        Center(
-          child: CircleAvatar(
-            radius: 100,
-            backgroundColor: Colors.green,
-            child: Text('data'),
-
-
-          ),
-        )
+        backgroundColor: Colors.green,
       ),
     );
-
   }
 }
