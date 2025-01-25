14d53ed7707e24ac6a2e47559eb0d3dc1b1d175c
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-01-25 15:43:43 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1496,7 +1496,7 @@ class MyApp extends StatelessWidget {
                 scrollDirection: Axis.horizontal,
                 child: Row(
                   children: [
-                    Text('Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.',style:
+                    Text('Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots ',style:
                       TextStyle(
                         fontWeight: FontWeight.bold,
                         color: Colors.amber
@@ -1505,7 +1505,7 @@ class MyApp extends StatelessWidget {
                       scrollDirection: Axis.horizontal,
                       child: Row(
                         children: [
-                          Text('Contrary to popular belief, Lorem Ipsum is not simply random text.,',style:
+                          Text('Contrary to popular belief, Lorem Ipsum is not simply random text.,',style: 
                             TextStyle(
                               fontWeight: FontWeight.bold,
                               color: Colors.deepPurple
