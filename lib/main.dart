d568168d01ca45df581b70847e0ce2855f207550
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-02-02 15:32:40 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -13,117 +13,120 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          body: 
-          Column(
-            children: [
-              Row(
-                children: [
-                  TextButton(onPressed: (){
-                    print('Button Pressed!');
-                  }, child: Text('Click')),
-                  TextButton(onPressed: (){
-                    print('Hello World');
-                  }, child: Text('For World')),
-                  TextButton(onPressed: (){
-                    print('Hello World');
-                  }, child: Text('For World')),
-                  TextButton(onPressed: (){
-                    print('Hello World');
-                  }, child: Text('For World')),
-                  TextButton(onPressed: (){
-                    print('Hello World');
-                  }, child: Text('For World')),
-                ],
-              ),
-              Row(
-                children: [
-                  TextButton(onPressed: (){
-                    print('Hello Button!');
-                  }, child: Text('Hello')),
-                  TextButton(onPressed: (){
-                    print('Hello World');
-                  }, child: Text('For World')),
-                  TextButton(onPressed: (){
-                    print('Hello World');
-                  }, child: Text('For World')),
-                  TextButton(onPressed: (){
-                    print('Hello World');
-                  }, child: Text('For World')),
-                  TextButton(onPressed: (){
-                    print('Hello World');
-                  }, child: Text('For World')),
-                ],
-              ),
-              Row(
-                children: [
-                  TextButton(onPressed: (){
-                    print('HELLO FLUTTER');
-                  }, child: Text('For Flutter')),
-                  TextButton(onPressed: (){
-                    print('Hello World');
-                  }, child: Text('For World')),
-                  TextButton(onPressed: (){
-                    print('Hello World');
-                  }, child: Text('For World')),
-                  TextButton(onPressed: (){
-                    print('Hello World');
-                  }, child: Text('For World')),
-                  TextButton(onPressed: (){
-                    print('Hello World');
-                  }, child: Text('For World')),
-                ],
-              ),
-              Row(
-                children: [
-                  TextButton(onPressed: (){
-                    print('Hello World');
-                  }, child: Text('For World')),
-                  TextButton(onPressed: (){
-                    print('Hello World');
-                  }, child: Text('For World')),
-                  TextButton(onPressed: (){
-                    print('Hello World');
-                  }, child: Text('For World')),
-                  TextButton(onPressed: (){
-                    print('Hello World');
-                  }, child: Text('For World')),
-                  TextButton(onPressed: (){
-                    print('Hello World');
-                  }, child: Text('For World')),
-                ],
-              ),
-              Row(
-                children: [
-                  TextButton(onPressed: (){
-                    print('Screen button click');
-                  },
-                      
-                      style: TextButton.styleFrom(
-                        padding: EdgeInsets.all(16.0),
-                        textStyle: TextStyle(fontSize: 20),
-                        backgroundColor: Colors.blue,
+        home: Messengerscreen();
+        // Scaffold(
+        //   body:
+        //   Column(
+        //     children: [
+        //       Row(
+        //         children: [
+        //           TextButton(onPressed: (){
+        //             print('Button Pressed!');
+        //           }, child: Text('Click')),
+        //           TextButton(onPressed: (){
+        //             print('Hello World');
+        //           }, child: Text('For World')),
+        //           TextButton(onPressed: (){
+        //             print('Hello World');
+        //           }, child: Text('For World')),
+        //           TextButton(onPressed: (){
+        //             print('Hello World');
+        //           }, child: Text('For World')),
+        //           TextButton(onPressed: (){
+        //             print('Hello World');
+        //           }, child: Text('For World')),
+        //         ],
+        //       ),
+        //       Row(
+        //         children: [
+        //           TextButton(onPressed: (){
+        //             print('Hello Button!');
+        //           }, child: Text('Hello')),
+        //           TextButton(onPressed: (){
+        //             print('Hello World');
+        //           }, child: Text('For World')),
+        //           TextButton(onPressed: (){
+        //             print('Hello World');
+        //           }, child: Text('For World')),
+        //           TextButton(onPressed: (){
+        //             print('Hello World');
+        //           }, child: Text('For World')),
+        //           TextButton(onPressed: (){
+        //             print('Hello World');
+        //           }, child: Text('For World')),
+        //         ],
+        //       ),
+        //       Row(
+        //         children: [
+        //           TextButton(onPressed: (){
+        //             print('HELLO FLUTTER');
+        //           }, child: Text('For Flutter')),
+        //           TextButton(onPressed: (){
+        //             print('Hello World');
+        //           }, child: Text('For World')),
+        //           TextButton(onPressed: (){
+        //             print('Hello World');
+        //           }, child: Text('For World')),
+        //           TextButton(onPressed: (){
+        //             print('Hello World');
+        //           }, child: Text('For World')),
+        //           TextButton(onPressed: (){
+        //             print('Hello World');
+        //           }, child: Text('For World')),
+        //         ],
+        //       ),
+        //       Row(
+        //         children: [
+        //           TextButton(onPressed: (){
+        //             print('Hello World');
+        //           }, child: Text('For World')),
+        //           TextButton(onPressed: (){
+        //             print('Hello World');
+        //           }, child: Text('For World')),
+        //           TextButton(onPressed: (){
+        //             print('Hello World');
+        //           }, child: Text('For World')),
+        //           TextButton(onPressed: (){
+        //             print('Hello World');
+        //           }, child: Text('For World')),
+        //           TextButton(onPressed: (){
+        //             print('Hello World');
+        //           }, child: Text('For World')),
+        //         ],
+        //       ),
+        //       Row(
+        //         children: [
+        //           TextButton(onPressed: (){
+        //             print('Screen button click');
+        //           },
+        //
+        //               style: TextButton.styleFrom(
+        //                 padding: EdgeInsets.all(16.0),
+        //                 textStyle: TextStyle(fontSize: 20),
+        //                 backgroundColor: Colors.blue,
+        //
+        //
+        //               ),
+        //               child: Text('Screen')),
+        //           TextButton(onPressed: (){
+        //             print('Hello World');
+        //           }, child: Text('For World')),
+        //           TextButton(onPressed: (){
+        //             print('Hello World');
+        //           }, child: Text('For World')),
+        //           TextButton(onPressed: (){
+        //             print('Hello World');
+        //           }, child: Text('For World')),
+        //           TextButton(onPressed: (){
+        //             print('Hello World');
+        //           }, child: Text('For World')),
+        //         ],
+        //       )
+        //     ],
+        //   )
+        // )
 
+    // );
+    }
 
-                      ),
-                      child: Text('Screen')),
-                  TextButton(onPressed: (){
-                    print('Hello World');
-                  }, child: Text('For World')),
-                  TextButton(onPressed: (){
-                    print('Hello World');
-                  }, child: Text('For World')),
-                  TextButton(onPressed: (){
-                    print('Hello World');
-                  }, child: Text('For World')),
-                  TextButton(onPressed: (){
-                    print('Hello World');
-                  }, child: Text('For World')),
-                ],
-              )
-            ],
-          )
-        )
-    );
-  }
 }
