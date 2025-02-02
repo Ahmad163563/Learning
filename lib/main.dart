8c25f7e436e865b13cb905a8eb4533fa5adc4cb5
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-02-02 15:24:20 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -17,19 +17,110 @@ class MyApp extends StatelessWidget {
           body: 
           Column(
             children: [
-              TextButton(onPressed: (){
-                print('Button Pressed!');
-              }, child: Text('Click')),
-              TextButton(onPressed: (){
-                print('Hello Button!');
-              }, child: Text('Hello')),
-              TextButton(onPressed: (){
-                print('HELLO FLUTTER');
-              }, child: Text('For Flutter')),
-              TextButton(onPressed: (){
-                print('Hello World');
-              }, child: Text('For World')),
-              Text
+              Row(
+                children: [
+                  TextButton(onPressed: (){
+                    print('Button Pressed!');
+                  }, child: Text('Click')),
+                  TextButton(onPressed: (){
+                    print('Hello World');
+                  }, child: Text('For World')),
+                  TextButton(onPressed: (){
+                    print('Hello World');
+                  }, child: Text('For World')),
+                  TextButton(onPressed: (){
+                    print('Hello World');
+                  }, child: Text('For World')),
+                  TextButton(onPressed: (){
+                    print('Hello World');
+                  }, child: Text('For World')),
+                ],
+              ),
+              Row(
+                children: [
+                  TextButton(onPressed: (){
+                    print('Hello Button!');
+                  }, child: Text('Hello')),
+                  TextButton(onPressed: (){
+                    print('Hello World');
+                  }, child: Text('For World')),
+                  TextButton(onPressed: (){
+                    print('Hello World');
+                  }, child: Text('For World')),
+                  TextButton(onPressed: (){
+                    print('Hello World');
+                  }, child: Text('For World')),
+                  TextButton(onPressed: (){
+                    print('Hello World');
+                  }, child: Text('For World')),
+                ],
+              ),
+              Row(
+                children: [
+                  TextButton(onPressed: (){
+                    print('HELLO FLUTTER');
+                  }, child: Text('For Flutter')),
+                  TextButton(onPressed: (){
+                    print('Hello World');
+                  }, child: Text('For World')),
+                  TextButton(onPressed: (){
+                    print('Hello World');
+                  }, child: Text('For World')),
+                  TextButton(onPressed: (){
+                    print('Hello World');
+                  }, child: Text('For World')),
+                  TextButton(onPressed: (){
+                    print('Hello World');
+                  }, child: Text('For World')),
+                ],
+              ),
+              Row(
+                children: [
+                  TextButton(onPressed: (){
+                    print('Hello World');
+                  }, child: Text('For World')),
+                  TextButton(onPressed: (){
+                    print('Hello World');
+                  }, child: Text('For World')),
+                  TextButton(onPressed: (){
+                    print('Hello World');
+                  }, child: Text('For World')),
+                  TextButton(onPressed: (){
+                    print('Hello World');
+                  }, child: Text('For World')),
+                  TextButton(onPressed: (){
+                    print('Hello World');
+                  }, child: Text('For World')),
+                ],
+              ),
+              Row(
+                children: [
+                  TextButton(onPressed: (){
+                    print('Screen button click');
+                  },
+                      
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.all(16.0),
+                        textStyle: TextStyle(fontSize: 20),
+                        backgroundColor: Colors.blue,
+
+
+                      ),
+                      child: Text('Screen')),
+                  TextButton(onPressed: (){
+                    print('Hello World');
+                  }, child: Text('For World')),
+                  TextButton(onPressed: (){
+                    print('Hello World');
+                  }, child: Text('For World')),
+                  TextButton(onPressed: (){
+                    print('Hello World');
+                  }, child: Text('For World')),
+                  TextButton(onPressed: (){
+                    print('Hello World');
+                  }, child: Text('For World')),
+                ],
+              )
             ],
           )
         )
