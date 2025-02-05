d30ed5239ff808c3038c6d1a61838362902c0700
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-02-05 11:38:27 +0500

    will see later

--- lib/whatsapp screen.dart
+++ lib/whatsapp screen.dart
@@ -7,21 +7,52 @@ class Whatsappscreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body:
-        Container(
-          height: 100,
-          width: 500,
-          color: Colors.green,
-          child: Row(
-            children: [
-              SizedBox(width: 15,),
-              Text('WhatsApp',style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
 
-              ),)
-            ],
-          ),
-        )
+      Column(
+        children: [
+          Container(
+              height: 170,
+              width: 500,
+              color: Colors.green,
+              child:
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(height: 140,),
+                      SizedBox(width: 15,),
+                      Text('WhatsApp',style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+
+                      ),
+                      ),
+                      SizedBox(width: 150,),
+                      Icon(Icons.search,color: Colors.white,),
+                      SizedBox(width: 31,),
+                      Icon(Icons.messenger, color: Colors.white,),
+                      SizedBox(width: 30,),
+                      Icon(Icons.more_vert,color: Colors.white,),
+                    ],
+                     
+                  ),
+                  Row(
+                    ,
+                    children: [
+                      SizedBox(width:20),
+                      Text('Calls',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),),
+                  SizedBox(width:20),
+                      Text('Chats',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),),
+                  SizedBox(width:20),
+                      Text('Contacts',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),),
+                    ],
+                  )
+                ],
+              ),
+
+            ),
+        ],
+      )
     );
   }
 }
