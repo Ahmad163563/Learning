e1ca8d3ed46b623f20dee5ba04286dc11e7536de
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-02-19 15:47:03 +0500

    new update

--- lib/atm.dart
+++ lib/atm.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/atmLogin.dart';
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
@@ -6,13 +7,20 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> {@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(Duration(seconds: 3),(){
+      Navigator.push(context, MaterialPageRoute(builder: (context)=>ATMScreen()))
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
     home: Scaffold(
       body: Center(
-        child: Image.asset('assests/atm.dart'),
+        child: Image.asset('assests/atm.png'),
       ),
     )
     );
