f5f63d5ff7d18060dcea7d97ab321b7ccb94554b
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-03-15 17:59:15 +0500

    update

--- lib/loopcode.dart
+++ lib/loopcode.dart
@@ -1,32 +1,45 @@
 import 'package:flutter/material.dart';
 class Loopcode extends StatefulWidget {
-  const Loopcode({super.key});
+   Loopcode({super.key});
 
   @override
   State<Loopcode> createState() => _LoopcodeState();
 }
 
 class _LoopcodeState extends State<Loopcode> {
+  List userName=[
+    'ahmad',
+    'atta',
+    'salman'
+    'kamran'
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-          itemBuilder:(context, index){
-          int i=index+1;
-          return 
-            Card(
-              color: Colors.orange,
-              child: ListTile(
-              leading: CircleAvatar(child: Text('$index'),),
-              title:Text('title-$index'),
-              subtitle:Text( 'data'),
-              trailing:Text( 'data'),
-                        ),
-            );
-          }
+      body:
+        ListView.builder(
+            itemCount:userName.length,
+          itemBuilder:(context,index){
+              return ListTile(
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>ChatViewScreen(name:userName[index])));
+                },
+                title: Text(userName[index]),
+              );
+          },
+        )
+    );
+  }
+}
+class ChatViewScreen extends StatelessWidget {
+  const ChatViewScreen({super.key});
 
-      )
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('data'),
+      ),
     );
   }
 }
