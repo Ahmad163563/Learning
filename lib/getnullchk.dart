b51572b76e7d82a7f09e33870ff420cdc57404f3
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-03-18 13:55:38 +0500

    upddd

--- lib/getnullchk.dart
+++ lib/getnullchk.dart
@@ -7,20 +7,41 @@ class GetNullCheck extends StatefulWidget {
 }
 
 class _GetNullCheckState extends State<GetNullCheck> {
+  TextEditingController TolaPriceController= TextEditingController();
+  TextEditingController TolaQuantityController= TextEditingController();
+  TextEditingController GramQuantityController =TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
           TextFormField(
+            keyboardType: TextInputType.number,
+            controller: TolaPriceController,
             decoration: InputDecoration(hintText: 'Tola Price'),
           ),
           TextFormField(
+            keyboardType: TextInputType.number,
+            controller: TolaQuantityController,
             decoration: InputDecoration(hintText: 'Tola Quantity'),
           ),
           TextFormField(
-            decoration: InputDecoration(hintText: 'Tola Quantity'),
+            keyboardType: TextInputType.number,
+            controller: GramQuantityController,
+            decoration: InputDecoration(hintText: 'Gram Quantity'),
           ),
+          FloatingActionButton(onPressed: (){
+            if(TolaQuantityController.text=="")
+            {
+              TolaQuantityController.text='0';
+            }
+            if(
+            GramQuantityController.text==""
+            )
+            {
+              GramQuantityController.text='0';
+            }
+          })
         ],
       )
     );
